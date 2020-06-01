82e64a5eb424685ed444c1feb1a2de69a34803dc
Ayomide Omolewa <ayomide_omolewa1@baylor.edu>
2020-06-01 13:24:05 -0500

    Initial Commit

---
+++ Artista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artista
+//
+//  Created by Ayomide Omolewa on 6/1/20.
+//  Copyright © 2020 Ayomide Omolewa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
