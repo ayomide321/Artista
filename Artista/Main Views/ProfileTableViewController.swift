796cd5913f48e40047155ee6770d6158ec46283d
Ayomide Omolewa <ayomide_omolewa1@baylor.edu>
2020-06-29 12:32:34 -0500

    Create dynamic right button for profile view

--- Artista/Main Views/ProfileTableViewController.swift
+++ Artista/Main Views/ProfileTableViewController.swift
@@ -16,6 +16,9 @@ class ProfileTableViewController: UITableViewController {
     @IBOutlet weak var finishRegistrationButtonOutlet: UIButton!
     @IBOutlet weak var purchaseHistoryBurronOutlet: UIButton!
     
+    //MARK: - Vars
+    var editBarButtonOutlet: UIBarButtonItem!
+    
     //MARK: - view Lifecycle
     
     override func viewDidLoad() {
@@ -27,7 +30,7 @@ class ProfileTableViewController: UITableViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        //check logged in status
+        checkLoginStatus()
     }
 
     // MARK: - Table view data source
@@ -37,10 +40,40 @@ class ProfileTableViewController: UITableViewController {
         return 3
     }
     
+    //MARK: - Helpers
+    
+    private func checkLoginStatus() {
+        
+        if MUser.currentUser() == nil {
+            createRightBarButton(title: "Login")
+        } else {
+            createRightBarButton(title: "Edit")
+        }
+    }
+    
+    private func createRightBarButton(title: String) {
+        
+        editBarButtonOutlet = UIBarButtonItem(title: title, style: .plain, target: self, action: #selector(rightBarButtonItemPressed))
+        
+        self.navigationItem.rightBarButtonItem = editButtonItem
+    }
+    
+    //MARK: - IBActions
+    
+    @objc func rightBarButtonItemPressed() {
+        
+        if editButtonItem.title == "Login" {
+            //SHOW LOGIN VIEW
+        } else {
+            //go to profile info
+        }
+    }
+    
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        
     }
     
 
